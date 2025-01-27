e04bae94ff546933976fa4f6e9f815b81eea257b
Sanzhar <omarkhanovsanzh@gmail.com>
2025-01-27 21:45:00 +0500

    lab3

--- lab3/functions1/13.py
+++ lab3/functions1/13.py
@@ -2,10 +2,24 @@ import random
 
 
 n = random.randint(1, 20)
-a = input("Hello! What is your name?")
-print(f"Well, {a}, I am thinking of a number between 1 and 20.")
-num = int(input("Take a guess"))
+print("Hello! What is your name?")
+name = input()
 
-while num != n:
-    pass
+print(f"Well, {name}, I am thinking of a number between 1 and 20.")
+print("Take a guess.")
+num = int(input())
+c = 1
+
+while True:
+    if n == num:
+        print(f"Good job, {name}! You guessed my number in {c} guesses!")
+        break
+    if num > n:
+        print("Your guess is too high")
+        c+=1
+    if num < n:
+        print("Your guess is too low")
+        c+=1
+    print("Take a guess")
+    num = int(input())
 
