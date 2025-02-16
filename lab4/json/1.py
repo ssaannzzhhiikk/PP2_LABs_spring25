d9e5aef075b27a9f8c2b76127c535a70e0659047
Sanzhar <omarkhanovsanzh@gmail.com>
2025-02-16 19:42:33 +0500

    lab4

--- lab4/json/1.py
+++ lab4/json/1.py
@@ -3,3 +3,11 @@ import json
 with open("sample-data.json", "r") as file:
     data = json.load(file)
 
+print("Interface Status")
+print(80*"=")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
+
+for i in range(3):
+    print(data["imdata"][i]["l1PhysIf"]["attributes"]["dn"], "                            ", data["imdata"][i]["l1PhysIf"]["attributes"]["fecMode"], " ", data["imdata"][i]["l1PhysIf"]["attributes"]["mtu"])
+
